ad3912cf6828b3a8911f21fba64e23df6d58baf3
Evgeniy <nosenko.evgeniy@gmail.com>
2021-12-14 21:29:58 +0200

    polls 6 tutorial

--- ecomm_shop/polls/models.py
+++ ecomm_shop/polls/models.py
@@ -1,3 +1,5 @@
+from datetime import timezone
+import datetime
 from django.db import models
 
 # Create your models here.
@@ -5,6 +7,10 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
